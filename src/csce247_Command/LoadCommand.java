9959a93b82aa3346510ecad24898494920c05edf
c4181 <cm4181@gmail.com>
2020-03-24 12:01:11 -0400

    Added package declaration

--- src/csce247_Command/LoadCommand.java
+++ src/csce247_Command/LoadCommand.java
@@ -1,20 +1,23 @@
+package csce247_Command;
+
 /**
  * @author Divine Walker
  * @version 1.0
  * @since 03-23-2020
  */
 public class LoadCommand implements Command {
-    private Document document;
+  private Document document;
 
-    /**
-     * 
-     * @param doc
-     */
-    public LoadCommand(Document doc){
-        this.document = doc;
-    }
+  /**
+   * 
+   * @param doc
+   */
+  public LoadCommand(Document doc) {
+    this.document = doc;
+  }
 
-    public void execute() {
-        document.load();
-    }
+  @Override
+  public void execute() {
+    document.load();
+  }
 }
